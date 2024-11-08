3f8fcd3ebebaa9aebccc8d1bc84bb7f662810a66
vm rubi <takarolevementesalgado@gmail.com>
2024-11-08 12:06:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Dont starve',
+    'Qual Personangem começa com a letra W?',
+    'Todos, tirando o Maxwell haha.'
+)
+
+criaCartao(
+    'Subnautica',
+    'Qual o maior leviatã do subnautica?',
+    'THE ONLY AND ONE FUCKING GARGANTHUA LEVIATHAN'
+)
+
+criaCartao(
+    'LoL',
+    'Que item comumente é feito com a mascará assustadora?',
+    'A reposta obviamente é angustia de liandry'
+)
+
+criaCartao(
+    'Terraria',
+    'Qual a arma mais forte do terraria?',
+    'Muitos podem pensar que é a zenith, e é ela mesmo haha.'
+)
